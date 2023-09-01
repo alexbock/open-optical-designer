686d2fb9b583c05e07ac4e00de075c533ba8b4a3
Alexander Bock <alexandermbock@gmail.com>
2023-09-01 00:09:56 -0400

    Start adding unit tests for math functions

--- tests/test-math.js
+++ tests/test-math.js
@@ -2,5 +2,11 @@
 
 function runMathTests(t) {
     t.group_name = "math";
-    t(() => 4, 2); // TODO
+    // Vector.magnitude
+    t(() => Vector.magnitude([0,0]), 0);
+    t(() => Vector.magnitude([5,0,0]), 5);
+    t(() => Vector.magnitude([0,-5]), 5);
+    t(() => Vector.magnitude([5, 5, 5, 5]), 10);
+    t(() => Math.ceil(Vector.magnitude([303.9, 700, 1900])), 2048);
+    // TODO add tests for other vector and matrix functions
 }
